4be032ed857776a83b4a78d1e9d872427ee937da
Caio Souza <160979584+cmoiss@users.noreply.github.com>
2025-01-11 19:09:43 -0300

    imprime resultado

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,43 @@
 const cep = document.querySelector('#cep');
 const botaoBuscar = document.querySelector('#botao-buscar');
+const campoResultado = document.querySelector(".campo-resultado");
 
-botaoBuscar.addEventListener('click', () => {
+botaoBuscar.addEventListener('click', async () => {
     console.log('Busca iniciada!');
     // console.log(`Valor do input: ${cep.value}`);
 
-    buscarCEP(cep.value);
+    const endereco = await buscarCEP(cep.value);   
+    
+    imprimirResultado(endereco);
 });
 
-buscarCEP = async cep => {
+const buscarCEP = async cep => {
     const urlAPI = `https://viacep.com.br/ws/${cep}/json/`;
     const resposta = await fetch(urlAPI);
-    const result = resposta.json()
+    const result = await resposta.json();
 
     console.log(resposta);
     console.log(result);
+
+    return result;
 }
+
+const imprimirResultado = (endereco) => {
+    const {logradouro, bairro, localidade: cidade, uf} = endereco;
+
+    const textoLogadouro = document.createElement("p");
+    textoLogadouro.innerHTML = `<b>Endereço:</b> ${logradouro}`;
+    campoResultado.appendChild(textoLogadouro);
+
+    const textoBairro = document.createElement("p");
+    textoBairro.innerHTML = `<b>Bairro:</b> ${bairro}`;
+    campoResultado.appendChild(textoBairro);
+
+    const textoCidade = document.createElement("p");
+    textoCidade.innerHTML = `<b>Cidade:</b> ${cidade}`;
+    campoResultado.appendChild(textoCidade);
+
+    const textoEstado = document.createElement("p");
+    textoEstado.innerHTML = `<b>Estado:</b> ${uf}`;
+    campoResultado.appendChild(textoEstado);    
+};
