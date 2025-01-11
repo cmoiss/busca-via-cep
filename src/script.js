d19b898ae03eef56a44cfef7d7084dd583d7b680
Caio Souza <160979584+cmoiss@users.noreply.github.com>
2025-01-11 17:05:47 -0300

    programa reconhece o input

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
+const cep = document.querySelector('#cep');
 const botaoBuscar = document.querySelector('#botao-buscar');
 
 botaoBuscar.addEventListener('click', () => {
   console.log('Botão clicado!');
+  console.log(`Valor do input: ${cep.value}`);
 });
